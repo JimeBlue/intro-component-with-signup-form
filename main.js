920cca84f9f65b38cbbc3632f0f40c3f6d64e9a9
Jimena <jimeblue69@gmail.com>
2022-08-11 03:41:06 +0200

    Delete comments

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-// Get the form
 const form = document.getElementById('form');
-// Get the first Name input
 const firstName = document.getElementById('first-name');
-// Get the last Name input
 const lastName = document.getElementById('last-name');
-// Get the email input
 const email = document.getElementById('email');
-// Get the password input
 const password = document.getElementById('password');
-
-// Add event listener to the form submition
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  // Call custom made function that checks the inputs
   checkInputs();
 });
 
-//Create a function that checks the inputs
-
 const checkInputs = () => {
-  // get values from the inputs, using the trim function to remove all white space
   const firstNameValue = firstName.value.trim();
   const lastNameValue = lastName.value.trim();
   const emailValue = email.value.trim();
@@ -29,19 +17,15 @@ const checkInputs = () => {
 
   // Validate first name
   if (firstNameValue === '') {
-    //If the first name input is empty call the setErrorFor function and pass the input to which we want to set the error, in this casae firstName and also pass an error message
     setErrorFor(firstName, 'First Name cannot be empty');
-    //Delete the placeholder
     firstName.placeholder = '';
   } else {
-    // if the first name input is not empty call the setSuccessFor function passing only the firstName input without any message
     setSuccessFor(firstName);
   }
 
   // Validate last name
   if (lastNameValue === '') {
     setErrorFor(lastName, 'Last Name cannot be empty');
-    //Delete the placeholder
     lastName.placeholder = '';
   } else {
     setSuccessFor(lastName);
@@ -50,9 +34,7 @@ const checkInputs = () => {
   // Validate email
   if (emailValue === '') {
     setErrorFor(email, 'Email cannot be empty');
-    //To change the placeholder text
     email.placeholder = 'email@example/com';
-    //Check if email is valid calling the isEmail function
   } else if (!isEmail(emailValue)) {
     setErrorFor(email, 'Looks like this is not a valid email');
   } else {
@@ -63,30 +45,21 @@ const checkInputs = () => {
 
   if (passwordValue === '') {
     setErrorFor(password, 'Password cannot be empty');
-    //Delete the placeholder
     password.placeholder = '';
   } else {
     setSuccessFor(password);
   }
 };
 
-// Create function to handle the error
 const setErrorFor = (input, message) => {
-  // Get the parent of the target input, i.e .form__item>
   const formItem = input.parentElement;
-  //   Get the <small> where the error message will be display
   const small = formItem.querySelector('small');
-  //   Show the error message
   small.innerText = message;
-  // Add class error to parent of error icon. The class error in the css will show the icon
   formItem.className = 'form__item error';
 };
 
-// Create function to show there is no error
 const setSuccessFor = (input) => {
-  // Get the parent of the target input, i.e .form__item
   const formItem = input.parentElement;
-  // Add class sucess to parent of error icon. The class success in the css will hide the icon
   formItem.className = 'form__item success';
 };
 
